df96c6c2de58451b47918282064504fe52e61027
Ruslan Babkin <ruslan.babkin01@gmail.com>
2023-01-20 11:27:18 +0200

    add validation fields

--- schemas/contact.js
+++ schemas/contact.js
@@ -1,9 +1,11 @@
 const Joi = require("joi");
 
 const contactSchema = Joi.object({
-  name: Joi.string().required(),
+  name: Joi.string().alphanum().required(),
   email: Joi.string().email().required(),
-  phone: Joi.string().required(),
+  phone: Joi.string()
+    .regex(/^[+]?[(]?[0-9]{3}[)]?[-\s.]?[0-9]{3}[-\s.]?[0-9]{4,6}$/)
+    .required(),
 });
 
 module.exports = contactSchema;
