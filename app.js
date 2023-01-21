dfb8176d83fa6ca236f756892f9be79c95aa5f9e
Ruslan Babkin <ruslan.babkin01@gmail.com>
2023-01-21 18:48:58 +0200

    add mongoose

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const { DB_HOST } = process.env;
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => console.log("Database connection successful"))
+  .catch((error) => console.log(error.message));
 
 const contactsRouter = require("./routes/api/contacts");
 
@@ -10,7 +21,7 @@ const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
 app.use(logger(formatsLogger));
 app.use(cors());
-// app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.use("/api/contacts", contactsRouter);
