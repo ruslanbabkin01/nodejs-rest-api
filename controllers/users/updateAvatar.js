6ace6923068b29f2ca3d5c9bc24922c31ee2e2de
Ruslan Babkin <ruslan.babkin01@gmail.com>
2023-02-07 11:22:18 +0200

    add update avatar

---
+++ controllers/users/updateAvatar.js
@@ -0,0 +1,22 @@
+const { User } = require("../../models");
+const fs = require("fs/promises");
+const path = require("path");
+
+const avatarsDir = path.join(__dirname, "../../", "public", "avatars");
+
+const updateAvatar = async (req, res) => {
+  const { path: tmpUpload, originalname } = req.file;
+  const { _id } = req.user;
+
+  const filename = `${_id}_${originalname}`;
+  const resultUpload = path.join(avatarsDir, filename);
+  await fs.rename(tmpUpload, resultUpload);
+  const avatarURL = path.join("avatars", filename);
+  await User.findByIdAndUpdate(_id, { avatarURL });
+
+  res.json({
+    avatarURL,
+  });
+};
+
+module.exports = updateAvatar;
