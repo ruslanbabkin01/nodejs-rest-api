b8a709fc99a8d7cf2f38454a1f2fe547e9e2de1a
Ruslan Babkin <ruslan.babkin01@gmail.com>
2023-03-14 08:36:20 +0200

    fixed add contact

--- controllers/contacts/add.js
+++ controllers/contacts/add.js
@@ -1,10 +1,10 @@
-const { Contact } = require("../../models");
+const { Contact } = require("../../models")
 
 const add = async (req, res) => {
-  const { _id } = req.user;
-  const result = await Contact.create({ ...req.body, owner: _id });
+  const { _id } = req.user
+  const result = await Contact.create({ ...req.body, owner: _id })
 
-  return res.status(201).json(result);
-};
+  res.status(201).json(result)
+}
 
-module.exports = add;
+module.exports = add
