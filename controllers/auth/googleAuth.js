d1d44834e458709de775a8ad84617b835a284405
Ruslan Babkin <ruslan.babkin01@gmail.com>
2023-05-17 12:25:12 +0300

    update googleAuth

--- controllers/auth/googleAuth.js
+++ controllers/auth/googleAuth.js
@@ -1,21 +1,18 @@
 require('dotenv').config()
 const jwt = require('jsonwebtoken')
+const { User } = require('../../models')
 
-const { FRONT_URL } = process.env
+const { FRONT_URL, ACCESS_SECRET_KEY, REFRESH_SECRET_KEY } = process.env
 
 const googleAuth = async (req, res) => {
   const { _id: id } = req.user
   const payload = { id }
 
-  // const accessToken = jwt.sign(payload, process.env.ACCESS_SECRET_KEY, {
-  //   expiresIn: '1m',
-  // })
-  const accessToken = jwt.sign(payload, process.env.ACCESS_SECRET_KEY, {
-    expiresIn: '1d',
-  })
-  const refreshToken = jwt.sign(payload, process.env.REFRESH_SECRET_KEY, {
+  const accessToken = jwt.sign(payload, ACCESS_SECRET_KEY, { expiresIn: '1d' })
+  const refreshToken = jwt.sign(payload, REFRESH_SECRET_KEY, {
     expiresIn: '7d',
   })
+  await User.findByIdAndUpdate(id, { accessToken, refreshToken })
 
   res.redirect(
     `${FRONT_URL}?accessToken=${accessToken}&refreshToken=${refreshToken}`
--- controllers/auth/login.js
+++ controllers/auth/login.js
@@ -25,9 +25,6 @@ const login = async (req, res) => {
     id: user._id,
   }
 
-  // const accessToken = jwt.sign(payload, process.env.ACCESS_SECRET_KEY, {
-  //   expiresIn: '1m',
-  // })
   const accessToken = jwt.sign(payload, process.env.ACCESS_SECRET_KEY, {
     expiresIn: '1d',
   })
